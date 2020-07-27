a85f13cde0cb82ee3403fd92892f64a7f88c7967
Tejashwini-Busnur <tejashwini.b.19@gmail.com>
2020-07-27 00:52:02 -0400

    to_do_list

---
+++ to_do_list.py
@@ -0,0 +1,17 @@
+to_do_list = []
+finished = False
+while not finished:
+    task = input("Enter a task for your to-do list. Press <enter> when done:")
+    if len(task) == 0:
+        finished = True
+    else:
+        to_do_list.append(task)
+        print("Task added.")
+
+print()
+print('Your To-Do List:')
+print('-' * 16)
+for task in to_do_list:
+    print(task)
+
+#to_do_list
