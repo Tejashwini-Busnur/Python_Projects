fc017e23f624233fa5c8464e9eb811ef3c3408f2
Tejashwini-Busnur <tejashwini.b.19@gmail.com>
2020-08-25 02:30:29 -0400

    DataDashboard Completed

---
+++ MobileApp/main.py
@@ -0,0 +1,23 @@
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.uix.screenmanager import ScreenManager, Scree
+
+
+Builder.load_file('design.kv')
+
+
+class LoginScreen(Screen):
+    pass
+
+
+class RootWidget(ScreenManager):
+    pass
+
+
+class MainApp(App):
+    def build(self):
+        return RootWidget()
+
+
+if __name__ == "__main__":
+    MainApp().run()
